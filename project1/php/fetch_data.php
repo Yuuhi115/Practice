2f6ccc6a7bb73142500158fbc20e6cfc9f17f48d
Yuuhi115 <1521427714@qq.com>
2024-07-17 14:36:06 +0800

    new file:   project1/php/fetch_data.php 	modified:   project1/showPage/main.html

---
+++ project1/php/fetch_data.php
@@ -0,0 +1,12 @@
+<?php
+    require('./database.php');
+        $list = mysqli_query($con,"SELECT * FROM product");
+        $product_array = array();
+        while($row = mysqli_fetch_assoc($list)){
+            $product_array[] = $row;
+        }
+        $response = json_encode($product_array);
+        echo $response;
+        mysqli_close($con);
+        
+?>
